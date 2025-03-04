1293aea3c82a46d5396f867dde3b86d2cef8bd42
halilyldz <arge4@tekviya.com>
2025-03-04 20:44:42 +0300

    Update MealsScreen to display a message when no meals are found; implement conditional rendering for meals list

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -9,6 +9,34 @@ class MealsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text(title)), body: Text('Body'));
+    Widget content = Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'No meals found.',
+            style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+              color: Theme.of(context).colorScheme.onSurface,
+            ),
+          ),
+          const SizedBox(height: 16),
+          Text(
+            'Try selecting a different category.',
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+              color: Theme.of(context).colorScheme.onSurface,
+            ),
+          ),
+        ],
+      ),
+    );
+
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (ctx, index) => Text(meals[index].title),
+      );
+    }
+
+    return Scaffold(appBar: AppBar(title: Text(title)), body: content);
   }
 }
